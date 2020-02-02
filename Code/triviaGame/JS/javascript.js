76740a8c4ff8f535fb250b92187da425e6b9b22e
Aragrohl <mortimerhedges@gmail.com>
2020-02-02 10:02:19 +0000

    Added folders for organization and basic code

---
+++ Code/triviaGame/JS/javascript.js
@@ -0,0 +1,19 @@
+/*
+Author Name: Mort Hedges
+Project Name: Who wants to win £1
+Date: 28/01/2020
+Module: Computing project
+Version: V1
+*/
+
+var questions = [], [];
+
+i = 0;
+j = 0;
+
+function readFile() {
+    for(/*Lines in csv file*/) {
+        
+    }
+};
+
