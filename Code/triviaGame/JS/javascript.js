577f4fdbb611ce22105ac22c3b02a9f6f52b2b8e
Aragrohl <mortimerhedges@gmail.com>
2020-02-10 11:50:44 +0000

    RNG, testing and additional functions

--- Code/triviaGame/JS/javascript.js
+++ Code/triviaGame/JS/javascript.js
@@ -1,19 +1,43 @@
 /*
-Author Name: Mort Hedges
+Author Name: 14 Eyes - Mort Hedges
 Project Name: Who wants to win £1
-Date: 28/01/2020
+Date: 10/02/2020
 Module: Computing project
 Version: V1
 */
 
-var questions = [], [];
+
+var questions = [[], [], [], [], []];
+
+questions.push("What is 2 squared?",true,1,2,4,7)
+
 
 i = 0;
 j = 0;
 
+/*
 function readFile() {
-    for(/*Lines in csv file*/) {
+    for() {
         
     }
 };
+*/
 
+function randomNum() {
+    var num = 0;
+    num = Math.floor((Math.random() * /*Length of question row*/) + 1);
+    return num;   
+}
+
+function processQuestions()
+
+function arrayAppend(difficulty,question) {
+
+    questions[difficulty].push(question)
+
+}
+
+function printQuestions() {
+
+    document.getElementById().textContent = questions;
+}
